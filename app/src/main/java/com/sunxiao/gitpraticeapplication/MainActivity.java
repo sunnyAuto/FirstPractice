c135d803e87b9be06a68b437893440fc10424ff5
sunnnyAuto <18247602177@163.com>
2018-01-02 15:27:42 +0800

    第七次修改：mainActivity增加三个log

--- app/src/main/java/com/sunxiao/gitpraticeapplication/MainActivity.java
+++ app/src/main/java/com/sunxiao/gitpraticeapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.sunxiao.gitpraticeapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private void initView(){
         //github上添加注释
         Log.e("log","init");
+        Log.e("log","init2");
+        Log.e("log","init3");
     }
 }
