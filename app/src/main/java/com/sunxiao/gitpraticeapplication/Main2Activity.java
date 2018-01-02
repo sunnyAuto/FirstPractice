13d2e98c1fb8678032fdedf815ba11f1813bc146
sunnnyAuto <18247602177@163.com>
2018-01-02 14:39:26 +0800

    第二次修改：在Main2中删除了initView

--- app/src/main/java/com/sunxiao/gitpraticeapplication/Main2Activity.java
+++ app/src/main/java/com/sunxiao/gitpraticeapplication/Main2Activity.java
@@ -11,10 +11,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        initView();
+
     }
 
-    private void initView() {
-        textView = (TextView) findViewById(R.id.tv);
-    }
 }
