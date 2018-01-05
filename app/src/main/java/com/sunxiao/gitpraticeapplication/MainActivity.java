657a97c899eee574a6cd0d7fd71a95398a0c71c8
sunnnyAuto <18247602177@163.com>
2018-01-05 17:23:54 +0800

    第十五次修改：dev加log

--- app/src/main/java/com/sunxiao/gitpraticeapplication/MainActivity.java
+++ app/src/main/java/com/sunxiao/gitpraticeapplication/MainActivity.java
@@ -3,8 +3,13 @@ package com.sunxiao.gitpraticeapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton to_top ;
+    private ScrollView scrollView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
     private void initView(){
         //github上添加注释
         Log.e("log","init");
+        to_top = (ImageButton) findViewById(R.id.to_top);
+        scrollView = (ScrollView) findViewById(R.id.scroll_view);
+        to_top.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scrollView.fullScroll(ScrollView.FOCUS_UP);
+            }
+        });
     }
 }
